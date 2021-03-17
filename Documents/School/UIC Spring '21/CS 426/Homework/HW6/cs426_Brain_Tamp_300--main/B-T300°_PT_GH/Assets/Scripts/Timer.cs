907719bd407a86e2b8e727ed8e0978089192f750
Abel Abraham <abelaj50@gmail.com>
2021-03-17 00:01:13 -0500

    Timer added.

---
+++ Documents/School/UIC Spring '21/CS 426/Homework/HW6/cs426_Brain_Tamp_300--main/B-T300°_PT_GH/Assets/Scripts/Timer.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour
+{
+
+    public GameObject textField;
+    float timeToWin = 40f;
+    float timeRemaining = 0f;
+    //maybe add static variable or NPC array here.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeRemaining = timeToWin;
+        textField.GetComponent<Text>().text = "Time left: 00:40";
+        
+    }
+
+    // Update is called once per frame  (
+    void Update()
+    {
+        
+        //Time calculation.
+        timeRemaining = timeRemaining - (1 * Time.deltaTime);
+        
+        
+        /* Bug: Fix bounds here, or implement helper statements, to make timer work correctly in program.
+         Current issue of showing as 00:010 and small delay between 00:01 and Game over! */
+        
+        //Different text output if the time remaining is greater than 10 seconsds.
+        if (timeRemaining >= 10) {
+            textField.GetComponent<Text>().text = "Time left: 00:" + timeRemaining.ToString("f0");
+        }
+        
+        //Different text output if the time remaining is less than 10 seconds, but greater than 0.
+        if (timeRemaining < 10 && timeRemaining > 0) {
+            textField.GetComponent<Text>().text = "Time left: 00:0" + timeRemaining.ToString("f0");
+        }
+        
+        //If time has ran out!
+        if(timeRemaining < 0) {
+            
+            textField.GetComponent<Text>().text = "Game over!";
+            
+            //Add "You lose!" game screen / scene here, & ending game code.
+            
+        }
+        
+        
+
+        
+    }
+    
+
+}
---
+++ Documents/School/UIC Spring '21/CS 426/Homework/HW6/cs426_Brain_Tamp_300--main/B-T300°_PT_GH/Assets/inventory.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class inventory : MonoBehaviour
+{
+    public UnityEvent donut1;
+    public UnityEvent donut2;
+    public UnityEvent donut3;
+    public UnityEvent load2;
+    public UnityEvent load3;
+    int inventoryMax;
+    int inventoryTotal;
+    int donuts;
+    bool hot;
+    bool stafe;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventoryMax = 3;
+        inventoryTotal = 3;
+        donuts = 3;
+        hot = true;
+        stafe = true;
+            load2.Invoke();
+            load3.Invoke();
+            load3.Invoke();
+    }
+
+    void removeDonut()
+    {
+        if(donuts == 3)
+        {
+            donut1.Invoke();
+        }
+        else if(donuts == 2)
+        {
+            donut2.Invoke();
+        }
+        else if(donuts == 1)
+        {
+            donut3.Invoke();
+        }//*/
+        if(donuts > 0)
+        {
+            donuts = donuts - 1;
+            inventoryTotal = inventoryTotal - 1;
+            //donut1.Invoke();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stafe)
+        {
+            stafe = false;
+        }
+        if (Input.GetKey(KeyCode.Q) && hot)
+        {
+            removeDonut();
+            hot = false;
+        }
+        else if(!Input.GetKey(KeyCode.Q))
+        {
+            hot = true;
+        }
+    }
+}
